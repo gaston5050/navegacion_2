74622fae12bc0e89e64e9414ea5c68d2aea70022
gaston5050 <80988908+gaston5050@users.noreply.github.com>
2026-01-05 16:50:45 -0300

    paso 3 stepDish ok

--- app/src/main/java/com/example/lunchtray/LunchTrayScreen.kt
+++ app/src/main/java/com/example/lunchtray/LunchTrayScreen.kt
@@ -15,21 +15,38 @@
  */
 package com.example.lunchtray
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
+import androidx.navigation.compose.rememberNavController
+import com.example.lunchtray.datasource.DataSource
+import com.example.lunchtray.ui.EntreeMenuScreen
 import com.example.lunchtray.ui.OrderViewModel
+import com.example.lunchtray.ui.SideDishMenuScreen
+import com.example.lunchtray.ui.StartOrderScreen
 
 // TODO: Screen enum
+
+enum class rutas(){
+    Start, EntreeMenu, SideDishMenu, AccompanimentMenu, Checkout
+}
+
 
 // TODO: AppBar
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun LunchTrayApp() {
+fun LunchTrayApp(navController: NavHostController = rememberNavController()) {
     // TODO: Create Controller and initialization
 
     // Create ViewModel
@@ -43,5 +60,59 @@ fun LunchTrayApp() {
         val uiState by viewModel.uiState.collectAsState()
 
         // TODO: Navigation host
+        NavHost(
+            navController = navController,
+            startDestination = rutas.Start.name,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            composable(rutas.Start.name) {
+                StartOrderScreen(
+                    onStartOrderButtonClicked = { navController.navigate(route = rutas.EntreeMenu.name) },
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(innerPadding)
+                )
+            }
+            composable(route = rutas.EntreeMenu.name) {
+
+                EntreeMenuScreen(
+                    options = DataSource.entreeMenuItems,
+                    onCancelButtonClicked = {
+                        viewModel.resetOrder()
+                        navController.popBackStack(rutas.Start.name, inclusive = false)
+
+                    },
+                    onNextButtonClicked = {
+                        navController.navigate(rutas.SideDishMenu.name)
+                    },
+                    onSelectionChanged = { item ->
+                        viewModel.updateEntree(item)
+                    }
+
+                )
+
+
+            }
+            composable(route = rutas.SideDishMenu.name) {
+                SideDishMenuScreen(
+                    options = DataSource.sideDishMenuItems,
+                    onCancelButtonClicked = {
+                        viewModel.resetOrder()
+                        navController.popBackStack(rutas.Start.name, inclusive = false)
+                    },
+                    onNextButtonClicked = {
+                        navController.navigate(rutas.AccompanimentMenu.name)
+                    },
+                    onSelectionChanged = { item ->
+                        viewModel.updateSideDish(item)
+                    }
+                )
+            }
+
+
+        }
+
+
     }
 }
+
